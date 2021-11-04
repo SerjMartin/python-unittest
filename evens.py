633d51af924af7ad1fef16a2948a07ca158c3a1f
SerjMartin <serjmartin@mail.com>
2021-11-04 16:03:53 +0000

    Add evens.py and test_evens.py file

---
+++ evens.py
@@ -0,0 +1,14 @@
+def even_number_of_evens(numbers):
+    """
+    Should raise a TypeError if a list is not passed into the function
+    error message: " A list was not passed into te function"
+    if number is empty return False
+    is the number of even numbers is odd, retur. False
+    is the number of even numbers is 0, return False
+    if the number of even numbrs ius even, return True
+    """
+
+    return None
+
+if __name__ == '__main__':
+    print(even_number_of_evens(5))
